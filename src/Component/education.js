66958c036d5e06e7e0f6a5246828b3caad3d0c64
CamilaMaira <camilamaira.m@gmail.com>
2020-02-10 12:47:13 -0300

    component educacion completo

---
+++ src/Component/education.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+
+function Education() {
+  const [nameInstitution, setnameInstitution] = useState('');
+  const [title, setTitle] = useState('');
+  const [since, setSince] = useState('');
+  const [until, setUntil] = useState('');
+  const [description, setDescription] = useState('');
+  const [area, setArea] = useState('');
+
+  return (
+    <div>
+      <p>Nombre Institución</p>
+      <input type="text" onChange={(event) => setnameInstitution(event.target.value)} value={nameInstitution} />
+
+      <p>Título</p>
+      <input type="text" onChange={(event) => setTitle(event.target.value)} value={title} />
+
+      <p>Desde</p>
+      <input type="date" onChange={(event) => setSince(event.target.value)} value={since} />
+
+      <p>Hasta</p>
+      <input type="date" onChange={(event) => setUntil(event.target.value)} value={until} />
+
+      <p>Descripción</p>
+      <input type="text" onChange={(event) => setDescription(event.target.value)} value={description} />
+
+      <p>Área</p>
+      <input type="text" onChange={(event) => setArea(event.target.value)} value={area} />
+
+
+    </div>
+  );
+}
+
+export default Education;
