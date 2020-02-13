a2ab85ba738796d4394e83901880ca9a932e59ca
CamilaMaira <camilamaira.m@gmail.com>
2020-02-13 10:42:55 -0300

    maquetado educacion

--- src/Views/education.js
+++ src/Views/education.js
@@ -1,1 +1,42 @@
+import React, { useState } from 'react';
+// import Education from '../Component/education';
+// import Education from '../Component/education';
 
+/* function educacionView() {
+
+  const [previewForm, setPreviewForm ] = useState('');
+  const addEducation = <Education />
+
+  let form;
+  if (previewForm) {
+    return (
+      <Educationtwo />
+      <button onClick={addEducation}>Agregar nueva Educación</button>
+      )
+
+  }else {
+    form =
+    <form>
+    <Education />
+    </form>
+  }
+
+  return (
+    <div>
+      {form}
+    </div>
+  );
+}
+
+export default educacionView; */
+
+
+function educacionView() {
+  return (
+    <div>
+      <p>hola</p>
+    </div>
+  );
+}
+
+export default educacionView;
