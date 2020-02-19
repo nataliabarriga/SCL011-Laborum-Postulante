8a567902305e516994c2078438916320d47cabf3
huenumanpaola03 <huenumanpaola03@gmail.com>
2020-02-19 12:35:02 -0300

    subo login con css y rutas de prueba para conectar con home y registry

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
 import './App.css';
-import Header from "./Views/Header"
+
+
+
+//aca estan las rutas de prueba para pasar el login 
+
+/* import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Views/Home';
+import Login from "./Component/Login";
+import React from "react";
+import RegistroPrueba from './Component/RegistroPrueba';
+ */
+
+
+/*
 
 function App() {
   return (
+
+
+
     <div className="App">
-      <Header/>
-    </div>
+      <header className='App-header'>
+
+  <BrowserRouter>
+      <React.Fragment>
+      <Route path='/Home' component={Home}/>
+      <Route path='/Login' component={Login}/>
+
+      <Route path='/RegistroPrueba' component={RegistroPrueba}/>
+      </React.Fragment>
+      </BrowserRouter>
+
+</header>
+  </div>
+   
   );
 }
 
 export default App;
+
+*/
---
+++ src/Views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Home = () => {
+    return (
+        <React.Fragment>
+          <div>probando home para login </div> 
+        </React.Fragment>
+    )           
+}
+
+export default Home;
