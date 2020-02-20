22d0bd091e1d80f258ca6bfa047f10e3da1b95b2
Camila Maira <54278381+CamilaMaira@users.noreply.github.com>
2020-02-20 18:27:52 -0300

    arreglo css login ok (#57)

--- src/Views/defaultComponent/Container.jsx
+++ src/Views/defaultComponent/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Container.css';
 import ProfileOne from "../ProfileOne"
 //import WorkOffers from '../../Component/workOffers'
-import ProfileOne from '../ProfileOne';
+
 
 const Container = () => {
   return (
