a145c00e90e2e4256ff75b892765cc1056dd984e
martcamila1 <53873117+martcamila1@users.noreply.github.com>
2020-02-20 14:00:09 -0300

    Feat/work offers (#56)
    * Aviso de trabajo y css
    
    * Arreglo css de workOffers
    
    Co-authored-by: Natalia Barriga <53873050+nataliabarriga@users.noreply.github.com>

--- src/Views/defaultComponent/Container.jsx
+++ src/Views/defaultComponent/Container.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Container.css';
+import ProfileOne from "../ProfileOne"
+//import WorkOffers from '../../Component/workOffers'
 import ProfileOne from '../ProfileOne';
 
 const Container = () => {
   return (
     <div className="col-8 container">
       <div>
+        /*<WorkOffers/>*/
         <ProfileOne />
       </div>
     </div>
