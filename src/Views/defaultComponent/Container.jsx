2efbf48bc613e84b436013370b5d6dd41e91ea49
Natalia Barriga <nbarrigam@MacBook-Air-de-Natalia.local>
2020-02-20 18:29:16 -0300

    para bajar

--- src/Views/defaultComponent/Container.jsx
+++ src/Views/defaultComponent/Container.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import './Container.css';
 import ProfileOne from "../ProfileOne"
 //import WorkOffers from '../../Component/workOffers'
+<<<<<<< HEAD
 
+=======
+>>>>>>> para bajar
 
 const Container = () => {
   return (
